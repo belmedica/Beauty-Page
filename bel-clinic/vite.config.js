adb406d50f1d40ae3f9a1ecdd2470cc96f73570c
Bel Medica <info@bel.clinic>
2026-01-15 21:47:01 -0500

    Corrigiendo rutas de despliegue

--- bel-clinic/vite.config.js
+++ bel-clinic/vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Beauty-Project/',
+  // IMPORTANTE: Esta línea le dice a la web que está dentro de la carpeta Beauty-Page
+  // Debe tener slash al principio y al final: /Nombre-Repo/
+  base: '/Beauty-Page/',
 })
